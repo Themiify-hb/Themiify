66af1d3acd602ead5615bc649ab072b23be5f31d
Fangal-Airbag <77993079+Fangal-Airbag@users.noreply.github.com>
2025-12-27 17:15:27 -0400

    Add copyright headers

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,11 @@
+/*
+ * Themiify - A theme manager for the Nintendo Wii U
+ * Copyright (C) 2026 Fangal-Airbag  
+ * Copyright (C) 2026 AlphaCraft9658
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
 #include <memory>
 
 #include <vpad/input.h>
