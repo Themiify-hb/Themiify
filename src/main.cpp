f7cf6f3e219c46ea667ef63f26910d811ff2adf6
Fangal-Airbag <fangalairbag@gmail.com>
2026-03-30 21:09:14 -0400

    Finalize CMake build system

--- src/main.cpp
+++ src/main.cpp
@@ -7,24 +7,49 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
-// Template for now cuz I'll actually start coding when I get home today lol
-#include <whb/proc.h>
 #include <whb/log.h>
-#include <whb/log_console.h>
+#include <whb/log_udp.h>
+
+#include <SDL2/SDL.h>
+#include <mocha/mocha.h>
 
 int main(int arc, char **argv)
 {
-    WHBProcInit();
-    WHBLogConsoleInit();
+    WHBLogUdpInit();
 
-    WHBLogPrint("Hello World!");
+    SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS);
 
-    while (WHBProcIsRunning()) {
-        WHBLogConsoleDraw();
+    SDL_Window *window = SDL_CreateWindow("Themiify", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1280, 720, SDL_WINDOW_SHOWN);
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+    WHBLogPrint("Hello world from themiify!");
+
+    bool isRunning = true;
+
+    while (isRunning) {
+        SDL_Event e;
+        while(SDL_PollEvent(&e)) {
+            switch (e.type) {
+                case SDL_QUIT:
+                    WHBLogPrint("buh bye!");
+                    isRunning = false;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
     }
+    
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
 
-    WHBLogConsoleFree();
-    WHBProcShutdown();
+    SDL_Quit();
+
+    WHBLogUdpDeinit();
 
     return 0;
 }
